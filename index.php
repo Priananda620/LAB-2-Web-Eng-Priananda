a8a259eb259f1628cdd2a85b7c912a9a1b84cb6c
Priananda Azhar <prianandaazhar2001@gmail.com>
2022-05-22 09:23:29 +0800

    3rd

--- index.php
+++ index.php
@@ -193,7 +193,7 @@
         </section>
 
 
-        <section id="section-2" class="section-child" style="padding: 3em 0;">
+        <section id="section-2" class="section-child py-5">
             <svg width="720" height="480" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
                 <defs>
                     <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
